44a4d944918cbeb7fadb31a2954b2cd466f84beb
AlekseiAnikeev <info.anikeev@gmail.com>
2022-03-21 22:21:46 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        String number = "0";
+        for (int i = 1; i <= 20000; i++) {
 
+            number += " " + i;
+        }
+        System.out.println(number);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("0");
+        for (int i = 1; i <= 20000; i++) {
+            sb.append(" ")
+                    .append(i);
+        }
+        System.out.println(sb);
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane ("Boeing", 2000, 150, 10000);
+        airplane.info();
+        System.out.println(airplane);
     }
 }
