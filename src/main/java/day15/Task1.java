e2bed785644a874a7da537af9dd2b78bfef61b95
AlekseiAnikeev <info.anikeev@gmail.com>
2022-03-30 19:23:00 +0300

    complete day15 tasks ending

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -15,12 +15,12 @@ public class Task1 {
                 if (list.length == 3) {
                     if (Integer.parseInt(list[2]) == 0)
                         writer.println(list[0] + ", " + list[1] + ", " + list[2]);
-                } else throw new IllegalArgumentException();
+                } else throw new IllegalArgumentException("Ошибка входного файла, проверьте соответствие столбцов");
             }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка входного файла, проверьте соответствие столбцов");
+            System.out.println(e.getMessage());
         }
     }
 }
