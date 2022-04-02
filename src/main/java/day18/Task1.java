466c61e1619174d83ce816f622938d2b4c6d9394
AlekseiAnikeev <info.anikeev@gmail.com>
2022-04-02 23:25:45 +0300

    complete day18 tasks ending

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -5,8 +5,10 @@ public class Task1 {
         int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
         System.out.println(recursionSum(numbers, 0));
     }
+
     public static int recursionSum(int[] numbers, int i) {
-        if (i == numbers.length) return 0;
-            return numbers[i] + recursionSum(numbers, i + 1);
-        }
+        if (i == numbers.length)
+            return 0;
+        return numbers[i] + recursionSum(numbers, i + 1);
+    }
 }
